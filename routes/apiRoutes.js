d101e02149423b408cf3afa9ba54c244d81b6c67
Jacob Dolph <jacob.dolph702@gmail.com>
2020-02-10 22:58:57 -0700

    generate a unique id,note  id  is base on the index number of the array object

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,22 @@
 
-var journalData = require("../db/journal.json");
+const fs = require("fs");
+const util = require("util");
 var dbData = require("../db/db.json");
 
 
 module.exports = function (app) {
-    app.get("/api/journal", function (req, res) {
-        res.json(journalData)
-    });
+
     app.get("/api/db", function (req, res) {
         res.json(dbData)
-    })
+    });
+    app.post("/api/db", function (req, res) {
+
+        console.log(req.body)
+        dbData.push(req.body)
+        res.json(true)
+    });
+    // app.delete("api/db/:id", function (req, res) {
+
+    // })
+
 }
